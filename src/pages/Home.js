75b6805549c6834ae88e8f4d5d80d2ee333038c4
unknown <yuheey3@yahoo.co.jp>
2021-09-25 00:06:53 -0400

    home page is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import{Link} from "react-router-dom"
+import BannerImage from '../assets/su.jpg'
+import "../styles/Home.css";
 
 function Home() {
     return (
-        <div>
-            Hello world
+        <div className ="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+          <div className="headerContainer">
+              <h1>Yuki's Sushi</h1>
+              <p>No.1 sushi restaurant in Toronto</p>
+              <Link to="/menu">
+              <button>ORDER NOW</button>
+              </Link>
+          </div>
         </div>
     )
 }
